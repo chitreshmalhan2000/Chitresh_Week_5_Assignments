54154b93e94b90590a06cd65d35ab78b7e994ce1
chitreshmalhan2000 <161840338+chitreshmalhan2000@users.noreply.github.com>
2026-04-10 14:07:39 +0530

    9th April Assignments uploaded

---
+++ 9th April Assignments/deploymentDemo/deploymentDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace deploymentDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
